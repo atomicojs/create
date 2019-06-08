3a22b7082761700e16eba60e9739a1264f438ef8
Uppercod <mtrujillo@outlook.cl>
2019-06-07 22:57:40 -0400

    the generation of bundles is improved through the plugins @atomico/rollup-pack

--- base/rollup-mjs/rollup.config.js
+++ base/rollup-mjs/rollup.config.js
@@ -1,58 +1,17 @@
-import inputHTML from "@atomico/rollup-plugin-input-html";
-import resolve from "rollup-plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import sucrase from "rollup-plugin-sucrase";
-import serve from "rollup-plugin-serve";
+import pack from "@atomico/rollup-pack";
+import babel from "rollup-plugin-babel";
 import importCss from "@atomico/rollup-plugin-import-css";
-import workbox from "@atomico/rollup-plugin-workbox";
-import sizes from "@atomico/rollup-plugin-sizes";
+// import workbox from "@atomico/rollup-plugin-workbox";
 
-let plugins = [
-	inputHTML(),
-	resolve({
-		extensions: [".js", ".ts"]
-	}),
-	importCss(),
-	sucrase({
-		production: true,
-		exclude: ["node_modules/**"],
-		jsxPragma: "h",
-		transforms: ["typescript", "jsx"]
-	}),
-	terser()
-];
-
-if (process.env.ROLLUP_WATCH) {
-	plugins.push(
-		serve({
-			open: true,
-			contentBase: "dist",
-			historyApiFallback: true
-		})
-	);
-} else {
-	plugins.push(
-		workbox({
-			globDirectory: "./dist",
-			globPatterns: ["index.html", "**/*.{js,css}"],
-			swDest: "./dist/sw.js",
-			navigateFallback: "index.html"
-		})
-	);
-}
-
-plugins.push(sizes());
-
-export default {
-	input: "*.html",
-	output: {
-		dir: "dist",
-		format: "esm",
-		sourcemap: true
-	},
-	plugins,
-	onwarn(message) {
-		if (/Circular dependency/gi.test(message)) return;
-		console.error(message);
-	}
-};
+export default pack("*.html", {
+	plugins: [
+		babel(),
+		importCss()
+		// workbox({
+		// 	globDirectory: "./dist",
+		// 	globPatterns: ["index.html", "**/*.{js,css}"],
+		// 	swDest: "./dist/sw.js",
+		// 	navigateFallback: "index.html"
+		// })
+	]
+});
