f0d090ebd1bfc9fd20d8ad72ac7cdd9597a37578
UpperCod <mtrujillo@outlook.cl>
2020-04-26 13:32:21 -0400

    replace download-git with git and group everything together to speed up the download

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "src/create.js",
+  output: {
+    file: "create.js",
+    format: "cjs",
+    banner: "#!/usr/bin/env node",
+  },
+  plugins: [resolve(), commonjs(), json()],
+};
---
+++ src/create.js
@@ -0,0 +1,111 @@
+import https from "https";
+import prompts from "prompts";
+import degit from "degit";
+
+let repo = "atomicojs/base";
+
+let branches = `https://api.github.com/repos/${repo}/branches`;
+
+function requestJson(url) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, { headers: { "user-agent": "node.js" } }, (res) => {
+        res.setEncoding("utf8");
+        let data = "";
+        res.on("data", (chunk) => (data += chunk));
+        res.on("end", () => resolve(JSON.parse(data)));
+      })
+      .on("error", reject);
+  });
+}
+
+function getLastVersion(npmInfo) {
+  return npmInfo["dist-tags"].latest;
+}
+
+async function getBranchs() {
+  let data = await requestJson(branches);
+  return Promise.all(
+    data
+      .filter((data) => data.name != "master")
+      .map(async (data) => {
+        let pkg = await requestJson(
+          `https://raw.githubusercontent.com/atomicojs/base/${data.name}/package.json`
+        );
+        return {
+          id: data.name,
+          pkg,
+        };
+      })
+  );
+}
+
+async function autorun() {
+  let branches = await getBranchs();
+
+  let defBranch;
+
+  console.log("\nWelcome to Atomico, let's create your project\n");
+
+  let fields = [
+    {
+      type: "text",
+      name: "folder",
+      message: "Project destination folder?",
+    },
+  ];
+
+  if (branches.length > 1) {
+    fields.push({
+      type: "select",
+      name: "branch",
+      message: "Select the type of project",
+      choices: branches
+        .sort((a, b) => (a.id > b.id ? 1 : -1))
+        .map(({ id, pkg }) => ({
+          ...pkg.meta,
+          value: id,
+        })),
+      initial: 0,
+    });
+  } else {
+    defBranch = branches[0];
+  }
+
+  let isCancel;
+
+  let data = await prompts(fields, {
+    onCancel() {
+      isCancel = true;
+    },
+  });
+  if (isCancel) return;
+
+  let message = await new Promise((resolve) => {
+    let branch = defBranch || data.branch;
+    let select = `github:${repo}${branch ? "#" + branch : ""}`;
+
+    const emitter = degit(select);
+
+    emitter.on("info", (info) => {
+      console.log(info.message);
+    });
+
+    emitter.clone(data.folder).then(() => {
+      resolve(
+        [
+          "",
+          `Your project has been created successfully, next steps:`,
+          `1. cd ./${data.folder}`,
+          `2. npm install`,
+          `3. npm run dev`,
+          `4. Enjoy Atomico!`,
+        ].join("\n")
+      );
+    });
+  });
+
+  console.log(message);
+}
+
+autorun();
